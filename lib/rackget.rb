9ef63a41e33b8b7f4c6f1cf2567eab627c49c674
John Hawthorn <john@hawthorn.email>
2026-02-22 08:05:40 -0800

    Initial commit

---
+++ lib/rackget.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rackget/version"
+
+module Rackget
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rackget/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rackget
+  VERSION = "0.1.0"
+end
